661b8393af4c46020dce33d44b39f9d418d63a9f
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-10-24 22:12:01 +0200

    [fix] compile testBinaryFon and remove example

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-g++ -std=c++20 src/example/main.cpp -O0 -ggdb -lyaml-cpp -lfmt src/demangle/demangle.cpp -isystem src -o example
+#g++ -std=c++20 src/example/main.cpp -O0 -ggdb -lyaml-cpp -lfmt src/demangle/demangle.cpp -isystem src -o example
 
 g++ -std=c++20 src/testYAMLFon/*.cpp -O0 -ggdb -lyaml-cpp -lfmt src/demangle/demangle.cpp src/catch/catch.cpp -isystem src -o testYAMLFon
+g++ -std=c++20 src/testBinaryFon/*.cpp -O0 -ggdb -lyaml-cpp -lfmt src/demangle/demangle.cpp src/catch/catch.cpp -isystem src -o testBinaryFon
